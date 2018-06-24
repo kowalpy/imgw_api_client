ec10d4582f5fd45e385f335a7aae2918aeeb72fb
kowalpy <mkov80@gmail.com>
2018-06-24 17:58:21 +0200

    added working code

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # imgw_api_client
-API client for IMGW public data retrieval
+API client for IMGW public data retrieval. By using IMGW API you accept aggreement: https://danepubliczne.imgw.pl/regulations
+
+Implementacja klienta API publicznych danych IMGW na licencji LGPL 3.0. 
+
+Komunikat IMGW:
+Korzystanie z Serwisu oznacza zgodę Użytkownika na przestrzeganie postanowień Regulaminu, dlatego też każdy Użytkownik zobowiązany jest do zapoznania się z treścią Regulaminu przed rozpoczęciem korzystania z Serwisu. https://danepubliczne.imgw.pl/regulations
---
+++ imgw_api_client.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+
+"""
+Implementacja klienta API publicznych danych IMGW na licencji LGPL 3.0.
+
+Wersja 1.0 - 24 czerwca 2018.
+Autor: [https://github.com/kowalpy|Marcin Kowalczyk]
+WWW: https://github.com/kowalpy/imgw_api_client
+
+Komunikat IMGW:
+Korzystanie z serwisu oznacza zgode uzytkownika na przestrzeganie postanowien regulaminu,
+dlatego tez kazdy uzytkownik zobowiazany jest do zapoznania sie z trescia regulaminu
+przed rozpoczeciem korzystania z serwisu. https://danepubliczne.imgw.pl/regulations
+"""
+
+import time
+import urllib
+import sys
+import optparse
+import ConfigParser
+
+class ImgwApiClient(object):
+    def __init__(self):
+        self.target_stations = []
+        self.contents = []
+
+    def read_remote_data(self):
+        myurl_pattern = "https://danepubliczne.imgw.pl/api/data/synop/id/%s/format/%s"
+        for i in self.target_stations:
+            myurl = myurl_pattern % (i['station'], i['format'])
+            response = urllib.urlopen(myurl)
+            response_decoded = response.read()
+            content = i
+            content['data'] = response_decoded
+            self.contents.append(content)
+
+    def save_data(self, filepath=""):
+        filename = str(time.time()) + ".txt"
+        try:
+            f = open(filename, 'w')
+            f.write(self.convert_contents_to_str())
+            f.close()
+        except Exception as e:
+            print "ERROR: %s" % e
+
+    def return_data(self):
+        return self.contents
+
+    def convert_contents_to_str(self):
+        cont_str = ""
+        for i in self.contents:
+            cont_str += i['station'] + "\n"
+            cont_str += i['format'] + "\n"
+            cont_str += i['data'] + "\n"
+        return cont_str
+
+    def print_data(self):
+        print self.convert_contents_to_str()
+
+    def read_ini_file(self, filepath="meteo.ini"):
+        config = ConfigParser.RawConfigParser()
+        config.read(filepath)
+        meteoformat = config.get("meteo", "format")
+        stations = config.get("meteo", "stations")
+        stations = stations.split(";")
+        for i in stations:
+            new_dict = {"station": i, "format": meteoformat}
+            self.target_stations.append(new_dict)
+
+class RunImgwApiClient(ImgwApiClient):
+    def __init__(self):
+        super(RunImgwApiClient, self).__init__()
+        self.warn = "imgw_api_client 1.0 - https://github.com/kowalpy/imgw_api_client\n\n"
+        self.warn += "Komunikat IMGW:\n"
+        self.warn += "Korzystanie z serwisu oznacza zgode uzytkownika na przestrzeganie"
+        self.warn += " postanowien regulaminu, dlatego tez kazdy uzytkownik zobowiazany"
+        self.warn += " jest do zapoznania sie z trescia regulaminu przed rozpoczeciem"
+        self.warn += " korzystania z serwisu. https://danepubliczne.imgw.pl/regulations\n\n"
+
+    def print_warning(self):
+        print(self.warn)
+
+    def run_standalone(self):
+        print(self.warn)
+        self.parse_args()
+        #TBD: futher proceeding based on input args, by default reading default ini file
+        self.read_ini_file()
+        self.read_remote_data()
+        self.print_data()
+        self.save_data()
+
+    def parse_args(self):
+        #TBD
+        pass
+
+def main():
+    RunImgwApiClient().run_standalone()
+
+if __name__ == '__main__':
+    main()
